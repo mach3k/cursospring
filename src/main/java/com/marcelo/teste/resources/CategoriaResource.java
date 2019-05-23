efb9f946655a610a75f7ec2a962aa9a9633e5bb1
Marcelo Ramos Machado <mr.machado@gmail.com>
2019-05-23 15:34:08 -0300

    Ajustes no endpoint categorias id

--- src/main/java/com/marcelo/teste/resources/CategoriaResource.java
+++ src/main/java/com/marcelo/teste/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.marcelo.teste.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,18 +21,5 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-	}
-	
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
 	}
 }
