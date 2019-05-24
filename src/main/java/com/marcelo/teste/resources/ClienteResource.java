5bc293d79462f7358e33c86dde53ae6114c5c4cd
Marcelo Ramos Machado <mr.machado@gmail.com>
2019-05-24 16:29:36 -0300

    EndPoint clientes

---
+++ src/main/java/com/marcelo/teste/resources/ClienteResource.java
@@ -0,0 +1,25 @@
+package com.marcelo.teste.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.teste.domain.Cliente;
+import com.marcelo.teste.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;  
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/marcelo/teste/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.marcelo.teste.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelo.teste.domain.Cliente;
+import com.marcelo.teste.repositories.ClienteRepository;
+import com.marcelo.teste.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
